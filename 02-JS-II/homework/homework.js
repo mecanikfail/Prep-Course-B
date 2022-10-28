8e42b6a7bba7c4bcf91893ba6074fa41cf4e775a
mecanikfail <hmantillace@yahoo.com>
2022-10-27 22:13:00 -0500

    Vamos Mejorando

--- 02-JS-II/homework/homework.js
+++ 02-JS-II/homework/homework.js
@@ -6,34 +6,49 @@
 
 function devolverString(str) {
     // "Return" la string provista: str
-    // Tu código:
+    // Tu código: 
+  var devolverString = "str"
+  return str;
   }
   
   function suma(x, y) {
     // "x" e "y" son números
     // Suma "x" e "y" juntos y devuelve el valor
     // Tu código:
+    var suma = x + y;
+    return suma;
   }
   
   function resta(x, y) {
     // Resta "y" de "x" y devuelve el valor
     // Tu código:
+    var resta = x - y;
+    return resta;
   }
   
   function multiplica(x, y) {
     // Multiplica "x" por "y" y devuelve el valor
     // Tu código:
+    var multiplica = x * y;
+    return multiplica;
   }
   
   function divide(x, y) {
     // Divide "x" entre "y" y devuelve el valor
     // Tu código:
+    var divide = x / y;
+    return divide;
   }
   
   function sonIguales(x, y) {
     // Devuelve "true" si "x" e "y" son iguales
     // De lo contrario, devuelve "false"
     // Tu código:
+    if (x == y) {
+      return true;
+  } else {
+          return false;
+      }
   }
   
   function tienenMismaLongitud(str1, str2) {
