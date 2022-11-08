3efba50b45a7ddd0d6bee8334a8f3551441eaa13
mecanikfail <hmantillace@yahoo.com>
2022-11-08 16:59:26 -0500

    ya estamos en sitonia

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,29 +8,41 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
+    let suma = 0;
+    for( let i = 1; i < 11; i++){
+      suma = suma + i
+    }
+    return suma
+
 }
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
+  return array.filter((e) => e % 2 === 0);
+
 }
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
+  return array.map ((num) => num ** 2);
 }
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
+  return array.reduce((prev, next)=> prev + next);
 }
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
+  var num = 8920 + "" 
+  return (num.length);
 }
   
   
